a3b97d72c650603d43c947692d51f33c47a85215
amckean12 <amckean@me.com>
2020-03-28 18:56:03 -0500

    works on style

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import MainContainer from './containers/main_container.jsx'
 import FooterContainer from './containers/footer_container.jsx'
 
 
+//Style Sheets
+import './app.scss'
+
 function App() {
   return (
     <div className="App">

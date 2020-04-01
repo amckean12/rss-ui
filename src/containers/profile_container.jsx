610caa887120a024ae18209b078ef1b0f5fc12bb
amckean12 <amckean@me.com>
2020-04-01 16:03:20 -0500

    adds layout to profile

--- src/containers/profile_container.jsx
+++ src/containers/profile_container.jsx
@@ -9,20 +9,26 @@ class ProfileContainer extends Component {
             <section className="profile row">
                 <div className="col-md-3 col-sm-12 profile__overview-container">
                     <div className="profile__overview-stat profile__overview-stat--article-count">
-                        
+                        <h3 className="profile__overview-stat-heading">Total Artices</h3>
+                        <p className="profile__overview-stat-value">10</p>
                     </div>
                     <div className="profile__overview-stat profile__overview-stat--article-img-count">
-                    
+                        <h3 className="profile__overview-stat-heading">Artices With Image</h3>
+                        <p className="profile__overview-stat-value">5</p>
                     </div>
                     <div className="profile__overview-stat profile__overview-stat--oldest-article">
-                    
+                        <h3 className="profile__overview-stat-heading">Oldest Article</h3>
+                        <p className="profile__overview-stat-value">Oldest Article</p>
                     </div>
                     <div className="profile__overview-stat profile__overview-stat--newest-article">
-                    
+                        <h3 className="profile__overview-stat-heading">Newest Article</h3>
+                        <p className="profile__overview-stat-value">Oldest Article</p>
                     </div>
                 </div> 
                 <div className="col-md-9 col-sm-12 profile__rss-feed-container">
                     <div className="profile__rss-feed">
+                        <div className="profile__rss-feed-toolbar">
+                        </div>
                     </div> 
                 </div> 
             </section>

25ecd044fb244ce30db4aa07b0d54f3a203f7587
amckean12 <amckean@me.com>
2020-04-01 09:44:29 -0500

    changes button color

--- src/components/form_component.jsx
+++ src/components/form_component.jsx
@@ -5,16 +5,16 @@ const FormComponent = (props) => {
     return(
         <section className="form-section">
             <form className="form">
-                <div class="form-group">
+                <div class="form-group form__input-group">
                     <label>User Name</label>
                      <input type="text" class="form-control" placeholder="User Name" />
                 </div>
-                <div class="form-group">
+                <div class="form-group form__input-group">
                      <label>Password</label>
                      <input type="password" class="form-control" placeholder="Password" />
                 </div>
-                <button type="submit" class="btn btn-black">Login</button>
-                <button type="submit" class="btn btn-secondary">Register</button>
+                <button type="submit" class="btn form__button--primary">Login</button>
+                <button type="submit" class="btn form__button--secondary">Register</button>
             </form>
         </section>
     )

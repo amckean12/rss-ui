0d1eff5b75c864af4eb4238d7970b66f8e3d3300
amckean12 <amckean@me.com>
2020-04-01 09:33:09 -0500

    Adds login form

--- src/components/form_component.jsx
+++ src/components/form_component.jsx
@@ -3,8 +3,20 @@ import React from 'react';
 
 const FormComponent = (props) => {
     return(
-        <div className={`${props.block}__inner`}>
-        </div>
+        <section className="form-section">
+            <form className="form">
+                <div class="form-group">
+                    <label>User Name</label>
+                     <input type="text" class="form-control" placeholder="User Name" />
+                </div>
+                <div class="form-group">
+                     <label>Password</label>
+                     <input type="password" class="form-control" placeholder="Password" />
+                </div>
+                <button type="submit" class="btn btn-black">Login</button>
+                <button type="submit" class="btn btn-secondary">Register</button>
+            </form>
+        </section>
     )
 }
 

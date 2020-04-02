aeda52ed727a61ab6bf885f44bea7b009164f9fa
amckean12 <amckean@me.com>
2020-04-02 12:07:58 -0500

    works on rss feed styling

--- src/components/story_component.jsx
+++ src/components/story_component.jsx
@@ -4,11 +4,20 @@ import React from 'react';
 const StoryComponent = (props) => {
     return(
         <section className={`${props.block}__story-section`}>
+            {props.image && 
+                <img src={props.story.img_src} alt={props.story.img_alt}></img>
+            }
             <h3 className={`${props.block}__story-title`}>
-                <a className={`${props.block}__story-link`} href={`https://www.google.com`} target="_blank">
-                    Hello World
+                <a className={`${props.block}__story-link`} href={props.story.article_link} target="_blank" rel="noopener noreferrer">
+                    {props.story.title}
                 </a>
             </h3>
+            <p>
+                { props.story.published_date }
+            </p>
+            <p>
+                {props.story.description}
+            </p>
         </section>
     )
 }

9eee7bf421c41db6239566e57a7951a40660f4cb
amckean12 <amckean@me.com>
2020-04-03 13:17:20 -0500

    works on responsiveness

--- src/components/overview-stat_component.jsx
+++ src/components/overview-stat_component.jsx
@@ -5,7 +5,7 @@ const OverviewStatComponent = (props) => {
     return(
         <div className={`profile__overview-stat profile__overview-stat--${props.element}`}>
             <h3 className="profile__overview-stat-heading">{props.title}</h3>
-            <p className="profile__overview-stat-value">{props.content}</p>
+            <p className={`profile__overview-stat-value profile__overview-stat-value--${props.modifier}`}>{props.content}</p>
         </div>
     )
 }

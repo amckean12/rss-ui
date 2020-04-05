1623a2277fee746b18f63f4bf4ef66fecab60bf7
amckean12 <amckean@me.com>
2020-04-05 14:25:35 -0500

    fixes margin issue

--- src/containers/main_container.jsx
+++ src/containers/main_container.jsx
@@ -7,7 +7,7 @@ import ProfileContainer from './profile_container.jsx'
 class MainContainer extends Component {
     render(){
         return(
-            <main className="main">
+            <main className="main container-fluid">
                 <LoginContainer />
                 <ProfileContainer />
             </main>

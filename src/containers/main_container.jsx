60a40380f181d5d0bd12c99ce053d5f041cca66d
amckean12 <amckean@me.com>
2020-04-05 11:39:11 -0500

    rearchitects the profile accounts for header

--- src/containers/main_container.jsx
+++ src/containers/main_container.jsx
@@ -1,14 +1,147 @@
+//Libraries
 import React, { Component } from 'react';
 
 //Container
-import ProfileContainer from './profile_container.jsx'
+import StoryComponent from '../components/story_component.jsx'
+
+// Component
+import OverviewStatComponent from '../components/overview-stat_component.jsx'
+
+// Stylesheets
+import '../styles/components/profile.scss'
 
 
 class MainContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            user: "",
+            article_count: "",
+            article_count_img: "",
+            oldest_article: "",
+            newest_article: "",
+            article_history: [
+                {
+                    id: 1,
+                    title: "This is Article",
+                    article_link: "https://www.google.com/",
+                    published_date: "2020-03-30T16:10:00.000Z",
+                    description: "sed do eiusmod tempor incididunt",
+                    image: true,
+                    img_src: "https://assets.bwbx.io/images/users/iqjWHBFdfxIU/iUqQrbWEDNrc/v0/800x600.jpg",
+                    img_alt: "Health Image", 
+                    display_date: '',
+                }
+            ]
+        };
+    }
+
+    componentDidMount() {
+        fetch("http://localhost:3001/users/1")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        user: result.user_data,
+                        article_count: result.article_count,
+                        article_count_img: result.article_count_img,
+                        oldest_article: result.oldest_article,
+                        newest_article: result.newest_article,
+                        article_history: result.article_history,
+                    });
+                },
+            (error) => {
+                this.setState({
+                    isLoaded: true,
+                    error
+                });
+            }
+        )
+    }
+
+    renderStories(articles) {
+        if (articles){
+            return articles.map( story => {
+                return(<StoryComponent block="profile" story={story}/>)
+            })
+        }
+    }
+
+    sortStories(order_type){
+        let sorted_stories = ""
+        if(order_type === 'date'){
+            sorted_stories = this.state.article_history.sort((a, b) => {
+                if(a.display_date < b.display_date) { return -1; }
+                if(a.display_date > b.display_date) { return 1; }
+                return 0;
+            })
+        } else if (order_type === 'title') {
+            sorted_stories = this.state.article_history.sort((a, b) => {
+                    if(a.title.toLowerCase() < b.title.toLowerCase()) { return -1; }
+                    if(a.title.toLowerCase() > b.title.toLowerCase()) { return 1; }
+                    return 0;
+                })
+        } else if (order_type === 'description') {
+            sorted_stories = this.state.article_history.sort((a, b) => {
+                if(a.description.toLowerCase() < b.description.toLowerCase()) { return -1; }
+                if(a.description.toLowerCase() > b.description.toLowerCase()) { return 1; }
+                return 0;
+            })
+        } else {
+            return
+        }
+        this.setState({
+            article_history: sorted_stories
+        })
+    }
+
     render(){
         return(
             <main className="main container">
-                <ProfileContainer />
+                <section className="profile row">
+                    <div className="col-md-3 col-sm-12 pr-md-0 profile__overview-container">
+                        <OverviewStatComponent 
+                            element={'article-count'}
+                            title={'Total Articles'}
+                            modifier={'orange'}
+                            content={this.state.article_count} />
+                        <OverviewStatComponent 
+                            element={'article-count'}
+                            title={'Total Articles Img'}
+                            modifier={'light-blue'}
+                            content={this.state.article_count_img} />
+                        <OverviewStatComponent 
+                            element={'article-count'}
+                            title={'Earliest Published Date'}
+                            modifier={'aqua'}
+                            content={this.state.oldest_article.display_date} />
+                        <OverviewStatComponent 
+                            element={'article-count'}
+                            title={'Latest Published Date'}
+                            modifier={'dark-blue'}
+                            content={this.state.newest_article.display_date} />
+                    </div>
+                    <div className="col-md-9 col-sm-12 profile__rss-feed-container">
+                        <div className="profile__rss-feed">
+                            <div className="profile__rss-feed-toolbar">
+                                <div class="btn-group">
+                                    <button type="button" class="btn dropdown-toggle profile__rss-toolbar-sort-button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Sort</button>
+                                    <div class="dropdown-menu dropdown-menu-right">
+                                        <button class="dropdown-item" type="button" onClick={() => this.sortStories('title')}>Title</button>
+                                        <button class="dropdown-item" type="button" onClick={() => this.sortStories('date')}>Published Date</button>
+                                        <button class="dropdown-item" type="button" onClick={() => this.sortStories('description')}>Description</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="profile__rss-feed-list">
+                                { this.renderStories(this.state.article_history) }
+                            </div>
+                        </div> 
+                    </div>  
+                </section>
             </main>
         );
     }

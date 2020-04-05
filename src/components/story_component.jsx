f9a2c53dbb11480908053eeb12a37e1e5c834de1
amckean12 <amckean@me.com>
2020-04-05 13:47:54 -0500

    Adds img

--- src/components/story_component.jsx
+++ src/components/story_component.jsx
@@ -4,9 +4,8 @@ import React from 'react';
 const StoryComponent = (props) => {
     return(
         <section className={`${props.block}__story-section`}>
-            {props.image && 
-                <img src={props.story.img_src} alt={props.story.img_alt}></img>
-            }
+            {props.story.image===true ? 
+            <img className="img-fluid" src={props.story.img_src} alt={props.story.img_alt}></img> : '' }
             <h3 className={`${props.block}__story-title`}>
                 <a className={`${props.block}__story-link`} href={props.story.article_link} target="_blank" rel="noopener noreferrer">
                     {props.story.title}
